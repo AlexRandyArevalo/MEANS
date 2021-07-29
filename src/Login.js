6eb10ccc2f1ac863e1582949e34638885de45765
ARA <87749841+AlexRandyArevalo@users.noreply.github.com>
2021-07-28 22:42:06 -0300

    Add files via upload

--- src/Login.js
+++ src/Login.js
@@ -1,18 +1,21 @@
 import logo from './acn-logo.svg';
 import swal from 'sweetalert'
 import React, { useState } from 'react'
+import { useHistory } from 'react-router-dom';
 
 
 function Login() {
-  const [eid, setUser] = useState('')
-  const [pass, setPass] = useState('')
+  const [eid, setUser] = useState('alex@accenture.com')
+  const [pass, setPass] = useState('111')
+
+  const history = useHistory();
 
   const handleValidate = (e) => {
     e.preventDefault()
-    if (eid === 'andy@com' && pass === '1212')
-      swal("Succesful login", "Welcome to Reactjs", "success")
+    if (eid == 'alex@accenture.com' && pass == '111')
+      history.push('/')
     else
-      swal("Wrong Creadentials", "Please try again", "error")
+      swal("Wrong Credentials", "Please try again", "error")
   }
   return (
     <div className="container-fluid Login">
@@ -27,7 +30,7 @@ function Login() {
               <div className="mb-4">
                 <input onChange={(e) => setPass(e.target.value)} type="password" value={pass} className="form-control" placeholder="*Password" />
               </div>
-              <button type="submit" className="btn btn-purple mb-4">Submit</button>
+              <button type="submit" className="btn_ btn-purple mb-4 w-100">Submit</button>
             </form>
             <div className="d-flex justify-content-center">
               <img src={logo} className="acn-logo" alt="logo" />
