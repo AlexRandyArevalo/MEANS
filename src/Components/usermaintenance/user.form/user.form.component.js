efadc408f4246e8ec0991e0dbe4685672e16a010
ARA <87749841+AlexRandyArevalo@users.noreply.github.com>
2021-08-25 22:59:13 -0300

    Form/Label Modification

--- src/Components/usermaintenance/user.form/user.form.component.js
+++ src/Components/usermaintenance/user.form/user.form.component.js
@@ -62,7 +62,7 @@ export default class UserForm extends Component {
                         this.setState({ loader: false }) // Show Loader
                     });
             }
-            return (<div className="col-md-12 pt-3" style={{ textAlign: "end" }}>
+            return (<div className="col-md-12 p-3 pe-4 pt-0" style={{ textAlign: "end" }}>
                 <button onClick={SaveUser} className="btn_ btn-purple">
                     <ClipLoader className="pe-2" color={'#fff'} loading={this.state.loader} size={10} />
                     &nbsp; Submit &nbsp;
@@ -73,33 +73,40 @@ export default class UserForm extends Component {
             <ToastProvider>
                 <div className="shadow-sm p-3 mb-4 bg-body rounded mt-4">
                     <h5 className="p-4 pb-0"> Application Form</h5>
-                    <div className="row m-3">
-                        <div className="col-md-7">
-                            <input onChange={this.hasChanges.bind(this, 'eid')} value={this.state.eid} type="email" className="form-control" placeholder="E.I.D" required />
+                    <div className="row border rounded p-3 m-4">
+                        <div className="col-md-7 mt-3">
+                            <label className="pb-1 fw-bold fs-13 ps-1">EID</label>
+                            <input onChange={this.hasChanges.bind(this, 'eid')} value={this.state.eid} type="email" className="form-control" placeholder="Enter your E.I.D" required />
                         </div>
                         <div className="col-md-5">
+                            <label className="pb-1 fw-bold fs-13 ps-1">Contact</label>
                             <div className="input-group">
                                 <input onChange={this.hasChanges.bind(this, 'prefix')} type="text" className="form-control" value="+63" style={{ maxWidth: "55px" }} />
-                                <input onChange={this.hasChanges.bind(this, 'phoneno')} value={this.state.phoneno} type="text" className="form-control" placeholder="___-___-__" required />
+                                <input onChange={this.hasChanges.bind(this, 'phoneno')} value={this.state.phoneno} type="text" className="form-control" placeholder="Enter contact number" required />
                             </div>
                         </div>
                         <div className="col-md-4 mt-4">
-                            <input onChange={this.hasChanges.bind(this, 'lname')} value={this.state.lname} type="text" className="form-control" placeholder="Last Name" required />
+                            <label className="pb-1 fw-bold fs-13 ps-1">Last name</label>
+                            <input onChange={this.hasChanges.bind(this, 'lname')} value={this.state.lname} type="text" className="form-control" placeholder="Enter last name" required />
                         </div>
                         <div className="col-md-4 mt-4">
-                            <input onChange={this.hasChanges.bind(this, 'fname')} value={this.state.fname} type="text" className="form-control" placeholder="First Name" required />
+                            <label className="pb-1 fw-bold fs-13 ps-1">First name</label>
+                            <input onChange={this.hasChanges.bind(this, 'fname')} value={this.state.fname} type="text" className="form-control" placeholder="Enter first name" required />
                         </div>
                         <div className="col-md-4 mt-4">
-                            <input onChange={this.hasChanges.bind(this, 'mname')} value={this.state.mname} type="text" className="form-control" placeholder="Middle Name" required />
+                            <label className="pb-1 fw-bold fs-13 ps-1">Middle name</label>
+                            <input onChange={this.hasChanges.bind(this, 'mname')} value={this.state.mname} type="text" className="form-control" placeholder="Enter middle name" required />
                         </div>
                         <div className="col-md-12 mt-4">
-                            <input onChange={this.hasChanges.bind(this, 'addr1')} value={this.state.addr1} type="text" className="form-control" placeholder="Address Line 1" required />
+                            <label className="pb-1 fw-bold fs-13 ps-1">Address line 1</label>
+                            <input onChange={this.hasChanges.bind(this, 'addr1')} value={this.state.addr1} type="text" className="form-control" placeholder="Enter address line 1" required />
                         </div>
-                        <div className="col-md-12 mt-4">
-                            <input onChange={this.hasChanges.bind(this, 'addr2')} value={this.state.addr2} type="text" className="form-control" placeholder="Address Line 2" />
+                        <div className="col-md-12 mt-4 mb-3">
+                            <label className="pb-1 fw-bold fs-13 ps-1">Address line 2</label>
+                            <input onChange={this.hasChanges.bind(this, 'addr2')} value={this.state.addr2} type="text" className="form-control" placeholder="Enter address line 2" />
                         </div>
-                        <SubmitButton />
                     </div>
+                    <SubmitButton />
                 </div>
                 <UserList ref={this.elemRef} />
             </ToastProvider >
